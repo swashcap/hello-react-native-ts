5c0af8ddf60a9159c08a3d31bcaa92ddd8c68028
Cory Reed <swashcap@gmail.com>
2019-05-24 15:03:11 -0700

    feat: add storybook

---
+++ storybook/index.ts
@@ -0,0 +1,18 @@
+import { AppRegistry } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+import { name as appName } from '../app.json';
+import './rn-addons';
+
+// import stories
+configure(() => {
+  require('./stories');
+}, module);
+
+// Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({});
+
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
+
+export default StorybookUIRoot;
