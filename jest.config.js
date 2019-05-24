f870efe1fb13947d74800cc0c309d332ce0e1d0c
Cory Reed <swashcap@gmail.com>
2019-05-24 11:16:07 -0700

    fix: correct TypeScript configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  cacheDirectory: '.jest/cache',
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$'
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@
  * @format
  */
 
-import React, {Component} from 'react';
+import React from 'react';
+import {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
