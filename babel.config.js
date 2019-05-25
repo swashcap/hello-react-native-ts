c22e6e0c30266849fffffd328d84ffe16994519d
Cory Reed <swashcap@gmail.com>
2019-05-25 08:52:52 -0700

    feat: wire up storybook to app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  plugins: [
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['NODE_ENV', 'STORYBOOK']
+      }
+    ]
+  ],
   presets: ['module:metro-react-native-babel-preset']
 };
