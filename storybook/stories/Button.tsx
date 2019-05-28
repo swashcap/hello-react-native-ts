6137aeb05bf11f2aa1fdda39e70438b78a6462f1
Cory Reed <swashcap@gmail.com>
2019-05-28 10:47:00 -0700

    feat(storybook): add knobs to button story

--- storybook/stories/Button.tsx
+++ storybook/stories/Button.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 
 import { Button } from '../../src/components/controls/Button';
 
-storiesOf('Button', module).add(
+const story = storiesOf('Button', module);
+
+story.addDecorator(withKnobs);
+
+story.add(
   'With text',
-  () => <Button onPress={action('Pressed')}>Button text</Button>,
+  () => (
+    <Button
+      children={text('Children', 'Button text')}
+      disabled={boolean('Disabled', false)}
+      onPress={action('Pressed')}
+    />
+  ),
   {
     notes: 'This is the base button component'
   }
