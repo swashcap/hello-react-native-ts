b90115fc6fb6d1cbb31123f31b39fb926ff11829
Cory Reed <swashcap@gmail.com>
2019-06-09 20:52:34 -0700

    feat: add more content to home screen

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -8,16 +8,26 @@ import {
 } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 
+import { Button } from '../controls/Button';
 import { Body } from '../text/Body';
 import { Code } from '../text/Code';
 import { Headline } from '../text/Headline';
 
 const styles = StyleSheet.create({
+  body: {
+    marginBottom: 10
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 20,
+    marginTop: 10
+  },
   container: {
     flex: 1
   },
   content: {
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
     paddingHorizontal: 30,
     paddingVertical: 15
   },
@@ -85,9 +95,32 @@ export class HomeScreen extends React.Component<
           />
           <View style={styles.content}>
             <Headline style={styles.headline}>Home</Headline>
-            <Body>
+            <Body style={styles.body}>
               This tests the pull-to-refresh-style{' '}
               <Code>&lt;Image&nbsp;/&gt;</Code> element.
+            </Body>
+            <View style={styles.buttonWrapper}>
+              <Button>Toggle</Button>
+            </View>
+            <Body style={styles.body}>
+              Praesent laoreet gravida fermentum. Maecenas consectetur, odio at
+              elementum vehicula, elit ante imperdiet enim, vel luctus leo metus
+              quis tortor. Suspendisse potenti.
+            </Body>
+            <Body style={styles.body}>
+              Nam eget purus mi. Maecenas eleifend tempus erat, eget fringilla
+              lorem venenatis et.
+            </Body>
+            <Body style={styles.body}>
+              Curabitur dui nisi, mattis non ultrices at, adipiscing eget
+              libero. Integer consequat quam vitae diam lacinia tristique. Donec
+              malesuada blandit feugiat. Class aptent taciti sociosqu ad litora
+              torquent per conubia nostra, per inceptos himenaeos. Praesent in
+              semper lacus. Vestibulum ut nulla non massa imperdiet condimentum.
+              Sed nulla tortor, tristique aliquam elementum id, ullamcorper sit
+              amet turpis. Donec volutpat viverra turpis, nec tristique dui
+              aliquet non. Maecenas justo lorem, vestibulum eu feugiat vitae,
+              ultrices nec ipsum.
             </Body>
           </View>
         </Animated.ScrollView>
