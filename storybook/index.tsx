2b3bef092b58d682bc242753fef003e42e2a70f4
Cory Reed <swashcap@gmail.com>
2019-10-12 13:29:37 -0700

    chore: upgrade to storybook@^5

--- storybook/index.tsx
+++ storybook/index.tsx
@@ -1,5 +1,10 @@
-import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
+import React from 'react';
+import { AppRegistry, SafeAreaView } from 'react-native';
+import {
+  addDecorator,
+  getStorybookUI,
+  configure
+} from '@storybook/react-native';
 
 import { name as appName } from '../app.json';
 import './rn-addons';
@@ -12,6 +17,10 @@ configure(() => {
   require('./stories/List');
   require('./stories/Text');
 }, module);
+
+// Wrap all stories with `SafeAreaView`
+// https://storybook.js.org/docs/addons/introduction/#storybook-decorators
+addDecorator((storyFn: Function) => <SafeAreaView>{storyFn()}</SafeAreaView>);
 
 // Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
