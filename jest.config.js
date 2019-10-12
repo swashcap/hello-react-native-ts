88372d536f2bab67952dd56bcbe9fc0bc89caa66
Cory Reed <swashcap@gmail.com>
2019-10-12 12:56:04 -0700

    fix: transform react-navigation
    This fixes App-test.tsx syntax errors.

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
     '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
     '\\.(ts|tsx)$': 'ts-jest'
   },
+  transformIgnorePatterns: [
+    // react-navigation@^2 isn't transpiled
+    '/node_modules/(!react-navigation*)/'
+  ],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$'
 };
